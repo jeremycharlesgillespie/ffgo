eed0a965d41edf1113af835b776afb7fc580c158
Jeremy Gillespie <metalgear386@googlemail.com>
2020-04-26 19:06:40 -0500

    Updated Enemy members to be uppercase because they will be exported once the enemy object is made.

--- src/enemy/enemy.go
+++ src/enemy/enemy.go
@@ -2,10 +2,17 @@ package enemy
 
 //Enemy : is a bad guy
 type Enemy struct {
-	name      string
-	hp        int
-	mp        int
-	attack    int
-	def       int
-	abilities []string
+	Name      string
+	Hp        int
+	Mp        int
+	Attack    int
+	Def       int
+	Speed     int
+	Abilities []string
+}
+
+//AttackPlayer : attacks the player
+func (p Enemy) AttackPlayer() string {
+	message := p.Name + " attacked!"
+	return message
 }
